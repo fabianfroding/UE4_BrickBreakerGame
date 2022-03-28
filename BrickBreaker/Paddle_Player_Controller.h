9522342148ad4008e82607fee0336cc0baae2a5d
fabian <fabian.froding@gmail.com>
2022-03-28 19:31:46 +0200

    implt paddle, gamemode and input controller

---
+++ BrickBreaker/Paddle_Player_Controller.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Paddle_Player_Controller.generated.h"
+
+//class ABall
+
+UCLASS()
+class BRICKBREAKER_API APaddle_Player_Controller : public APlayerController
+{
+	GENERATED_BODY()
+
+		APaddle_Player_Controller();
+
+	UFUNCTION()
+		virtual void SetupInputComponent() override;
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	void MoveHorizontal(float AxisValue);
+
+	// Below is ball references varaibles
+	
+};
